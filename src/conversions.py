c2c165bc03a6d547983c425a92c8e9bff9cd68b6
chasenut <switalamaciej5@gmail.com>
2025-07-30 23:11:18 +0200

    regex url sourcing

--- src/conversions.py
+++ src/conversions.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextType, TextNode
 from htmlnode import LeafNode
 
@@ -17,3 +18,33 @@ def text_node_to_html_node(text_node):
         case TextType.IMAGE:
             return LeafNode("img", value, {"src": text_node.url})
     raise Exception("Error, invalid TextNode.text_type of class TextType")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        l = []
+        text = node.text
+        while len(text.split(delimiter, maxsplit=2)) > 2:
+            phrases = text.split(delimiter, maxsplit=2)
+            if phrases[0]:
+                l.append(TextNode(phrases[0], TextType.TEXT))
+            l.append(TextNode(phrases[1], text_type))
+            text = phrases[2]
+        if text:
+            if delimiter in text:
+                raise SyntaxError("Invalid Markdown syntax Error")
+            l.append(TextNode(text, TextType.TEXT))
+        new_nodes.extend(l)
+    return new_nodes
+
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    return re.findall(pattern, text)
+
+def extract_markdown_links(text):
+    pattern = r"\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    return re.findall(pattern, text)
+
--- src/test_conversions.py
+++ src/test_conversions.py
@@ -1,5 +1,10 @@
 import unittest
-from conversions import text_node_to_html_node
+from conversions import (
+    text_node_to_html_node,
+    split_nodes_delimiter, 
+    extract_markdown_images, 
+    extract_markdown_links,
+)
 from textnode import TextNode, TextType
 
 class TestConversions(unittest.TestCase):
@@ -14,3 +19,50 @@ class TestConversions(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.props, {"href": "https://www.google.com"})
+
+    def test_delim_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        correct_new_nodes = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, correct_new_nodes)
+
+    def test_telim_bold(self):
+        node = TextNode("This is text with **double** thic **bolds** texts within it", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        correct_new_nodes = [
+            TextNode("This is text with ", TextType.TEXT),
+            TextNode("double", TextType.BOLD),
+            TextNode(" thic ", TextType.TEXT),
+            TextNode("bolds", TextType.BOLD),
+            TextNode(" texts within it", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, correct_new_nodes)
+
+    def test_extract_markdown_image(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) ![image](https://github.com/chasenut/TF2-Sentry/blob/main/images/cad1.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png"), ("image", "https://github.com/chasenut/TF2-Sentry/blob/main/images/cad1.png")], matches)
+
+    def test_extract_markdown_link(self):
+        matches = extract_markdown_links(
+            "This is text with an [link](https://www.google.com)"
+        )
+        self.assertListEqual([("link", "https://www.google.com")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with an [link](https://www.google.com) ![link](https://github.com/chasenut)"
+        )
+        self.assertListEqual([("link", "https://www.google.com"), ("link", "https://github.com/chasenut")], matches)
+
